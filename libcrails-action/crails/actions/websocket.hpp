9fc31a90583e8e2770cbd49c0ff0894804ce82f0
plaristote <michael@unetresgrossebite.com>
2022-09-04 11:23:23 +0200

    adds websocket action

---
+++ libcrails-action/crails/actions/websocket.hpp
@@ -0,0 +1,39 @@
+#ifndef  CRAILS_WEBSOCKET_ACTION_HPP
+# define CRAILS_WEBSOCKET_ACTION_HPP
+
+# include <crails/context.hpp>
+# include <boost/beast/websocket.hpp>
+
+namespace Crails
+{
+  template<typename WEBSOCKET>
+  class WebSocketRoute
+  {
+    typedef void (WEBSOCKET::*Method)();
+  public:
+    static void trigger(Crails::Context& context, Method method, std::function<void()> callback)
+    {
+      const HttpRequest& request = context.connection->get_request();
+
+      if (boost::beast::websocket::is_upgrade(request))
+      {
+        auto websocket = std::make_shared<WEBSOCKET>(context);
+
+        context.response.set_status_code(HttpStatus::switching_protocols);
+        websocket->accept(request);
+        (websocket.get()->*method)();
+      }
+      else
+        context.response.set_status_code(HttpStatus::bad_request);
+      callback();
+    }
+  };
+}
+
+# define match_websocket(path, websocket, action) \
+  match("GET", path, [](Crails::Context& context, std::function<void()> callback) \
+  { \
+    WebSocketRoute<websocket>::trigger(context, &websocket::action, callback); \
+  });
+
+#endif
--- libcrails-action/crails/router.hpp
+++ libcrails-action/crails/router.hpp
@@ -6,6 +6,8 @@
 # include <crails/utils/singleton.hpp>
 # include <crails/context.hpp>
 # include <crails/router_base.hpp>
+# include "actions/controller.hpp"
+# include "actions/websocket.hpp"
 
 namespace Crails
 {
@@ -18,56 +20,6 @@ namespace Crails
   public:
     void initialize(void);
   };
-
-  template<typename CONTROLLER>
-  class ActionRoute
-  {
-    typedef void (CONTROLLER::*Method)();
-  public:
-    static void trigger(Crails::Context& context, Method method, std::function<void()> callback)
-    {
-      auto controller = std::make_shared<CONTROLLER>(context);
-
-      if (!context.response.sent())
-      {
-        controller->callback = std::bind(&ActionRoute<CONTROLLER>::finalize, controller.get(), callback);
-        controller->initialize();
-        if (!context.response.sent())
-          (controller.get()->*method)();
-      }
-      else
-        callback();
-      controller->close_on_deletion = true;
-    }
-
-  private:
-    static void finalize(CONTROLLER* controller, std::function<void()> callback)
-    {
-      controller->finalize();
-      callback();
-    }
-  };
 }
 
-# define SYM2STRING(sym) std::string(#sym)
-
-# define match_action(method, path, controller, action) \
-  match(method, path, [](Crails::Context& context, std::function<void()> callback) \
-  { \
-    context.params["controller-data"]["name"]   = #controller; \
-    context.params["controller-data"]["action"] = #action; \
-    ActionRoute<controller>::trigger(context, &controller::action, callback); \
-  })
-
-# define crud_actions(resource_name, controller) \
-   match_action("GET",    '/' + SYM2STRING(resource_name),               controller,index)  \
-  .match_action("GET",    '/' + SYM2STRING(resource_name) + "/:id" ,     controller,show)   \
-  .match_action("POST",   '/' + SYM2STRING(resource_name),               controller,create) \
-  .match_action("PUT",    '/' + SYM2STRING(resource_name) + "/:id",      controller,update) \
-  .match_action("DELETE", '/' + SYM2STRING(resource_name) + "/:id",      controller,destroy)
-
-# define resource_actions(resource_name, controller) \
-   match_action("GET",    '/' + SYM2STRING(resource_name) + "/new",      controller,new_)   \
-  .match_action("GET",    '/' + SYM2STRING(resource_name) + "/:id/edit", controller,edit)   \
-  .crud_actions(resource_name, controller)
 #endif
